a3313d8bf1396e3d985bd99deecfa31ba151b3b9
Lawrence Tung <ltung@users.noreply.github.com>
2018-04-30 12:11:14 -0700

    Update yqd.py

--- yahoo_quote_download/yqd.py
+++ yahoo_quote_download/yqd.py
@@ -37,7 +37,7 @@ _crumb = None
 
 # Headers to fake a user agent
 _headers={
-	'User-Agent': 'Mozilla/5.0 (X11; U; Linux i686) Gecko/20071127 Firefox/2.0.0.11'
+	'User-Agent': 'Mozilla/5.0 (Windows NT x.y; Win64; x64; rv:10.0) Gecko/20100101 Firefox/10.0'
 }
 
 def _get_cookie_crumb():
